6f127b9c056eb1baa252d0e693028a94b055ce4f
bradsimpson213 <bradsimpson@icloud.com>
2024-07-30 13:28:32 -0400

    updated for while loop code

---
+++ 03-while-loops.py
@@ -0,0 +1,27 @@
+# LOOPING / ITERATING
+
+# print("I love pizza!")
+# print("I love pizza!")
+# print("I love pizza!")
+
+# WHILE LOOPS
+
+# traditional while loop
+index=0
+
+while index < 6:
+    print("I love pizza!")
+    index += 1
+index = 0
+
+# While loop with continue and break
+while True:
+    if index < 3:
+        print("I love pizza!")
+        index += 1
+        continue
+
+    break
+
+
+
