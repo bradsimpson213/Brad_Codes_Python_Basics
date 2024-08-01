a12e8b2b2f4570f62c46f967218832d9c66c52ab
bradsimpson213 <bradsimpson@icloud.com>
2024-08-01 15:17:20 -0400

    uploaded for loop code

---
+++ 05-for-loops.py
@@ -0,0 +1,13 @@
+# FOR IN LOOPS
+colors = ["red", "orange", "blue", "green", "purple"]
+shapes = ("circle", "square", "rectangle", "triangle")
+
+# for color in colors:
+#     print(color)
+
+for shape in shapes:
+    print(shape)
+
+print("orange" in colors)
+print("yellow" in colors)
+print("circle" in shapes)
---
+++ 06-for-loops-advanced.py
@@ -0,0 +1,7 @@
+# FOR LOOPS ADVANCED
+
+colors = ["red", "orange", "blue", "green", "purple"]
+
+
+for color in colors:
+    print(color)
