47b1b8ec6f06e2b954aa606d3a90d0495b73d27a
bradsimpson213 <bradsimpson@icloud.com>
2024-08-01 16:19:41 -0400

    uploaded advanced for loops

--- 06-for-loops-advanced.py
+++ 06-for-loops-advanced.py
@@ -3,5 +3,20 @@
 colors = ["red", "orange", "blue", "green", "purple"]
 
 
-for color in colors:
-    print(color)
+# for color in colors:
+#     print(color)
+
+# print(range(5))
+# print(list(range(5)))
+# range(start, end, step)
+# print(list(range(0, 7)))
+# print(list(range(10, 0, -1)))
+
+# for index in range(len(colors)):
+#     print(index, colors[index])
+
+# print(enumerate(colors))
+# print(list(enumerate(colors)))
+
+for index, color in enumerate(colors, 1):
+    print(index, color)
