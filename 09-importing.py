b27cf1991b967a2e0fd45a3438042ad97d350022
bradsimpson213 <bradsimpson@icloud.com>
2024-09-24 19:18:18 -0400

    added importing video code

--- 09-importing.py
+++ 09-importing.py
@@ -1,6 +1,6 @@
 # IMPORTING
 # import random
-from random import randint, sample, choice
+from random import randint, sample, choice as picky
 from helper1 import add_2, add_4
 # from helpers.helper2 import add_6, add_8, names
 from helpers import add_6, add_8, add_10, names
@@ -12,3 +12,8 @@ print(add_6(2))
 print(add_8(2))
 print(names)
 print(add_10(2))
+
+def choice():
+    return "yes a choice"
+
+picky(['1', '2'])
